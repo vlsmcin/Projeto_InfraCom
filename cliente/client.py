bab9ffae6fd4215452a48135b2339c1d25c8a401
vlsmcin <viniciuslimamelo7@gmail.com>
2025-07-04 15:16:01 -0300

    cliente quase comentado

--- cliente/client.py
+++ cliente/client.py
@@ -8,11 +8,15 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 fileName = 'ex.txt'
 
+# Ler partes do arquivo (1024 bytes) para dividir no pacote
+# Manda primeiro o nome do arquivo e depois manda o arquivo
 packagesCount = 0
 with open(f'./arquivos_para_enviar/{fileName}', 'rb') as f:
     clientSocket.sendto(fileName.encode(), (serverName, serverPort))
     fileContent = f.read(1024)
+    # Continua no loop até não ter mais o que ler
     while fileContent:
+        # Não precisa ter encode pois lê em binário
         clientSocket.sendto(fileContent, (serverName, serverPort))
         packagesCount+=1
         fileContent = f.read(1024)
