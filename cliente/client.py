ba0311201edad121a4011b7ba79c658f7f4e82a7
rafaelpf <rpf@cin.ufpe.br>
2025-07-05 15:49:28 -0300

    Co-authored-by: João Pedro Marins <joaopbmarins@users.noreply.github.com> Co-authored-by: Vinícius Lima Sá de Melo <vlsmcin@users.noreply.github.com> Co-authored-by: Daniel Silvestre de França e Souza <dsfs10@users.noreply.github.com>

--- cliente/client.py
+++ cliente/client.py
@@ -1,16 +1,17 @@
 from socket import *
 
+# Dados do servidor (porta e ip)
 serverName = 'localhost'
 
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-fileName = 'ex.txt'
+fileName = 'teste.jpg'
 
 # Ler partes do arquivo (1024 bytes) para dividir no pacote
 # Manda primeiro o nome do arquivo e depois manda o arquivo
-packagesCount = 0
+packagesCount = 0 # Contador de pacotes para facilitar recebimento do arquivo vindo do servidor
 with open(f'./arquivos_para_enviar/{fileName}', 'rb') as f:
     clientSocket.sendto(fileName.encode(), (serverName, serverPort))
     fileContent = f.read(1024)
@@ -24,12 +25,15 @@ with open(f'./arquivos_para_enviar/{fileName}', 'rb') as f:
 
 print('Message successfully sent')
 
+# Recebe o nome do arquivo do servidor
 fileName, serverAddress = clientSocket.recvfrom(1024)
-fileName = fileName.decode()
+fileName = 'received_' + fileName.decode()
 
 for i in range(packagesCount):
+    # Lê cada pacote, iterando o número de pacotes que foi contado ao enviar
     modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
 
+    # Arquivo em modo a (append) para adicionar ao final do arquivo os bytes
     with open(f'./arquivos_recebidos_cliente/{fileName}', 'ab') as f:
         f.write(modifiedMessage)
 
--- servidor/server.py
+++ servidor/server.py
@@ -1,41 +1,47 @@
 from socket import *
 import os
 
+# Inicialização do servidor, tendo a porta, o ip e tipo (UDP)
 serverPort = 12000
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-serverSocket.bind(('localhost', serverPort))
+serverSocket.bind(('', serverPort))
 
 print ('The server is ready to receive')
 
 metadata = True
 
+# Loop infinito que o servidor executa esperando pacotes
 while True:
+    # Lê o inicialmente o nome do arquivo e setta metadata para False, indicando que agora de fato
+    # É o arquivo que está sendo enviado
     if metadata:
         filename, clientAddress = serverSocket.recvfrom(1024)
         metadata = False
         filename = filename.decode()
         name, extension = os.path.splitext(filename)
         file_count = 1
-        while os.path.exists(f'./arquivos_recebidos_servidor/{name}' + f'_{file_count}' + extension):
+        # Verifica se o arquivo já existe, e se existir, incrementa o contador para criar um nome único
+        while os.path.exists(f'./arquivos_recebidos_servidor/archived_{name}' + f'_{file_count}' + extension):
             file_count += 1
-        filename = name + f'_{file_count}' + extension
+        filename = 'archived_' + name + f'_{file_count}' + extension
         
     message, clientAddress = serverSocket.recvfrom(1024)
     modifiedMessage = message
 
+    # Abre (ou cria) o arquivo no modo append binário e escreve o bloco de dados
+    # Está no modo append para cada pacote incrementar as informações do arquivo
     with open(f'./arquivos_recebidos_servidor/{filename}', 'ab') as f:
         f.write(modifiedMessage)
 
+    # Se o tamanho do pacote recebido for menor que 1024 bytes, indica que é o último pacote
     if len(message) < 1024:
         serverSocket.sendto(filename.encode(), clientAddress)
+        # Abre o arquivo salvo no modo leitura binária e envia seu conteúdo de volta ao cliente em blocos de 1024 bytes
         with open(f'./arquivos_recebidos_servidor/{filename}', 'rb') as f:
             returnMessage = f.read(1024)
             while returnMessage:
                 serverSocket.sendto(returnMessage, clientAddress)
                 returnMessage = f.read(1024)
-        metadata = True
-
-# Se pedirem múltiplos clientes, criar uma thread ou simplesmente juntar com o ip
-# Seria bom a thread, ai manda como parametro pra thread o filename e o ip do cliente
+        metadata = True #Flag de controle para saber quando deve ler o nome de um arquivo
