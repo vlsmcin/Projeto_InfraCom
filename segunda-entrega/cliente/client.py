d8371ed83767f979f178d7f427248fc6654ef3b5
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2025-07-23 21:11:34 -0300

    organizando o codigo do cliente e do servidor

--- segunda-entrega/cliente/client.py
+++ segunda-entrega/cliente/client.py
@@ -3,47 +3,43 @@ import os, sys
 from math import ceil
 from fsm import *
 
-# Dados do servidor (porta e ip)
-serverName = 'localhost'
-
-serverPort = 12000
-
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-fileName = 'ex.txt'  
-
-# Ler partes do arquivo (1024 bytes) para dividir no pacote
-# Manda primeiro o nome do arquivo e depois manda o arquivo
-with open(f'./arquivos_para_enviar/{fileName}', 'rb') as f:
-    # Conta quantos pacotes tem a serem enviados, dividindo cada pacote em 1024 bytes
-    packagesCount = ceil(os.path.getsize(f'./arquivos_para_enviar/{fileName}') / 1024)
-    # Manda o nome e quantidade de pacotes
-    FSM_transmissor([fileName.encode(), packagesCount.to_bytes(4, byteorder='big')],clientSocket, (serverName, serverPort))
-    fileContent = f.read(1020)#1024
-
-    # Continua no loop até não ter mais o que ler
-    dados = []
-    while fileContent:
-        dados.append(fileContent)
+def send(fileName, clientSocket):
+    with open(f'./arquivos_para_enviar/{fileName}', 'rb') as f:
+        packagesCount = ceil(os.path.getsize(f'./arquivos_para_enviar/{fileName}') / 1024)
+        FSM_transmissor([fileName.encode(), packagesCount.to_bytes(4, byteorder='big')],clientSocket, (serverName, serverPort))
         fileContent = f.read(1020)
-    FSM_transmissor(dados, clientSocket, (serverName, serverPort))
+        dados = []
+        while fileContent:
+            dados.append(fileContent)
+            fileContent = f.read(1020)
+        FSM_transmissor(dados, clientSocket, (serverName, serverPort))
 
 
-print('Message successfully sent')
+    print(f'Arquivo {fileName} enviado para o servidor')
 
-# Recebe o nome do arquivo do servidor
+def receive(clientSocket):
+    pkt_fileName, serverAddress = FSM_receptor(clientSocket)
+    filename = 'received_' + pkt_fileName[0].decode()
 
-pkt_fileName, serverAddress = FSM_receptor(clientSocket)
-filename = 'received_' + pkt_fileName[0].decode()
+    data, clientAddress = FSM_receptor(clientSocket)
 
-data, clientAddress = FSM_receptor(clientSocket)
+    for i in data:
+        with open(f'./arquivos_recebidos_cliente/{filename}', 'ab') as f:
+            f.write(i)
 
-for i in data:
-    with open(f'./arquivos_recebidos_cliente/{filename}', 'ab') as f:
-        f.write(i)
+    print(f"Arquivo {filename} recebido")
 
-print("Arquivo recebido")
 
-clientSocket.close()
+if __name__ == '__main__':
+    # Dados do servidor (porta e ip)
+    serverName = 'localhost'
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    fileName = 'ex.txt'
+
+    send(fileName, clientSocket)
+    receive(clientSocket)
+
+    clientSocket.close()
 
 
--- segunda-entrega/servidor/server.py
+++ segunda-entrega/servidor/server.py
@@ -2,22 +2,7 @@ from socket import *
 import os, sys
 from fsm import *
 
-
-# Inicialização do servidor, tendo a porta, o ip e tipo (UDP)
-serverPort = 12000
-
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-
-serverSocket.bind(('', serverPort))
-
-print ('The server is ready to receive')
-
-
-# Loop infinito que o servidor executa esperando pacotes
-while True:
-    # Lê o inicialmente o nome do arquivo e setta metadata para False, indicando que agora de fato
-    # É o arquivo que está sendo enviado
-
+def receive(serverSocket):
     pkt_fileName, clientAddress = FSM_receptor(serverSocket)
     filename = pkt_fileName[0].decode()
     name, extension = os.path.splitext(filename)
@@ -30,7 +15,7 @@ while True:
     # Recebe informações da quantidade de pacotes para determinar parada
     packageCountEncoded = pkt_fileName[1]
     packageCount = int.from_bytes(packageCountEncoded, byteorder='big')
-        
+
     message, clientAddress = FSM_receptor(serverSocket)
     modifiedMessage = message
 
@@ -38,16 +23,36 @@ while True:
         with open(f'./arquivos_recebidos_servidor/{filename}', 'ab') as f:
             f.write(i)
 
-    #Reenviando o arquivo para o cliente
+    print(f"Recebeu o arquivo: {filename}")
+
+    return filename, packageCount, clientAddress
+
+
+def send(filename, packageCount, clientAddress, serverSocket):
     FSM_transmissor([filename.encode(), packageCount.to_bytes(4, byteorder='big')],serverSocket, clientAddress)
 
     dados = []
     with open(f'./arquivos_recebidos_servidor/{filename}', 'rb') as f:
         fileContent = f.read(1020)#1024
 
-        # Continua no loop até não ter mais o que ler
         while fileContent:
-            # Não precisa ter encode pois lê em binário
             dados.append(fileContent)
             fileContent = f.read(1020)
     FSM_transmissor(dados, serverSocket, clientAddress)
+
+    print(f"Enviou o arquivo: {filename}")
+
+
+if __name__ == '__main__':
+    # Inicialização do servidor, tendo a porta, o ip e tipo (UDP)
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
+    print ('The server is ready to receive')
+
+    while True:
+        fileName, packageCount, ClientAddress = receive(serverSocket)
+        send(fileName, packageCount, ClientAddress, serverSocket)
+
+
+
