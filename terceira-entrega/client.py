0fda4df604d8d9d01e023c41e0289bdeeac2eafd
rafaelpf <rpf@cin.ufpe.br>
2025-08-09 18:32:42 -0300

    Co-authored-by: João Pedro Marins <joaopbmarins@users.noreply.github.com>

--- terceira-entrega/client.py
+++ terceira-entrega/client.py
@@ -1,35 +1,81 @@
 import os
-from math import ceil
 from fsm import *
+import threading
 
 serverName = 'localhost'
 serverPort = 12000
 
-def send(clientSocket):
-    msg = input()
+isConnected = False
+isConnected_lock = threading.Lock()
 
-    if msg.startswith(":HI"):
-        login = msg[3:]
-        FSM_transmissor([b"HI", login.encode()], clientSocket, (serverName, serverPort))
+def splitMessage(texto, tamanho=1020):
+    return [texto[i:i+tamanho].encode() for i in range(0, len(texto), tamanho)]
+
+def send(clientSocket):
+    global isConnected
+    global serverPort
+    msg = str(input())
+
+    # Join chat session
+    if msg.startswith(":HI"): 
+        with isConnected_lock:
+            if not isConnected:
+                login = msg[4:] # Message format: ":HI <login>"
+                FSM_transmissor([b"HI", login.encode()], clientSocket, (serverName, serverPort))
+
+                pkt, _ = FSM_receptor(clientSocket)
+
+                if pkt[0].decode() == "ERROR":
+                    print(pkt[1].decode())
+                elif pkt[0].decode() == "OK":
+                    isConnected = True
+                    newPort = int.from_bytes(pkt[1], byteorder='big')
+                    serverPort = newPort # client will talk with server by new port
+                    print("Você entrou na sala de chat")
+                    print("Nova porta:", newPort)
+                    t1 = threading.Thread(target=receive)
+                    t1.start()
+            else:    
+                print("Você já está conectado")
+            return
+    # Exit chat session
+    elif msg.startswith(":BYE"):
+  
+        FSM_transmissor([b"BYE"], clientSocket, (serverName, serverPort))
 
         pkt, _ = FSM_receptor(clientSocket)
-
+     
         if pkt[0].decode() == "ERROR":
             print(pkt[1].decode())
         elif pkt[0].decode() == "OK":
-            newPort = int.from_bytes(pkt[1], byteorder='big')
-            print("Nova porta:", newPort)
+            with isConnected_lock:
+                isConnected = False
+            serverPort = 12000 # client will talk with server by new port
+            print("Você saiu da sala de chat")
+    else:
+        splitedMessage = splitMessage(msg)
+        FSM_transmissor(splitedMessage, clientSocket, (serverName, serverPort))
 
 
     return
 
-def receive(clientSocket):
-    return
+def receive():
+    global isConnected
+    while True:
+        with isConnected_lock:
+            if isConnected:
+                clientSocket = socket(AF_INET, SOCK_DGRAM)
+                clientSocket.bind(('', serverPort-1))
+
+                pkt, _  = FSM_receptor(clientSocket)
+                msg = [i.decode() for i in pkt]
+                msg = ''.join(msg)
+                print(msg)
+            else:
+                return
 
 if __name__ == '__main__':
     clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-    send(clientSocket)
-    receive(clientSocket)
-
-    clientSocket.close()
+    while True:
+        send(clientSocket)
--- terceira-entrega/server.py
+++ terceira-entrega/server.py
@@ -1,21 +1,69 @@
 import os
+import threading, queue
+from datetime import datetime
 from fsm import *
 
-# def receive(serverSocket):
+serverPort = 12000
+clientList = {"SERVIDOR":('', serverPort)} #   "login" : (ip,porta)
+clientList_lock = threading.Lock()
+
+msgQueue = queue.Queue(maxsize=50)
+
+def splitMessage(texto, tamanho=1020):
+    return [texto[i:i+tamanho].encode() for i in range(0, len(texto), tamanho)]
+
+def receiveMsgClient(clientIP, newPort):
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', newPort))
+
+    clientAddress = (clientIP, newPort)
+
+    while True:
+        pkt, realClientAddress = FSM_receptor(serverSocket) # get dynamic client port # TO DO: fix this
+        
+        if pkt[0].decode() == "BYE":
+            clientName = ""
+            with clientList_lock:
+                clientName = [cltName for cltName, cltAdress in clientList.items() if cltAdress == clientAddress]
+            clientList.pop(clientName[0])
+            print(f"Cliente {clientName[0]} pediu para sair da sala")
+            FSM_transmissor([b"OK"], serverSocket, realClientAddress)
+            return
+        else:
+            clientName = ""
+            msg = [i.decode() for i in pkt]
+            msg = ''.join(msg)
+            with clientList_lock:
+                clientName = [cltName for cltName, cltAdress in clientList.items() if cltAdress == clientAddress]
+            message = f'{clientAddress[0]}:{clientAddress[1]}/~{clientName[0]}: <{msg}> <{datetime.now().strftime("%H:%M:%S, %d/%m/%Y")}>'
+            print(message)
+            splitedMessage = splitMessage(message)
+            msgQueue.put(splitedMessage)
+
+def broadcast():
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
     
+    while True:
+        if msgQueue.qsize() > 0:
+            msg = msgQueue.get()
+            clientList_copy = {}
+            with clientList_lock:
+                clientList_copy = clientList.copy()
 
+            clientList_copy.pop("SERVIDOR")
 
-# def send(clientAddress, serverSocket):
-    
-
+            for k,v in clientList_copy.items():
+                FSM_transmissor(msg, serverSocket, (v[0],v[1]-1)) # Same IP but Port -1
 
 if __name__ == '__main__':
-    # Inicialização do servidor, tendo a porta, o ip e tipo (UDP)
-    serverPort = 12000
+    # Inicialização do servidor, tendo a porta, o ip e tipo (UDP)    
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
-    clientList = {"SERVIDOR":('', serverPort)} #   "login" : (ip,porta)
+    
     print ('The server is ready to receive')
+
+    t0 = threading.Thread(target=broadcast)
+    t0.start()
 
     while True:
         pkt, clientAdress = FSM_receptor(serverSocket)
@@ -24,11 +72,15 @@ if __name__ == '__main__':
             clientLogin = pkt[1].decode()
             if clientLogin not in clientList.keys():
                 lastKey, lastValue = list(clientList.items())[-1]
-                newPort = lastValue[1] + 1
+                newPort = lastValue[1] + 2
                 clientList[clientLogin] = (clientAdress[0], newPort)
                 print("Login recebido:", clientLogin)
                 FSM_transmissor([b"OK", newPort.to_bytes(4, byteorder='big')], serverSocket, clientAdress)
-                #create thread receber mensagem
+                # create thread receber mensagem
+                t1 = threading.Thread(target=receiveMsgClient, args=(clientAdress[0], newPort))
+                t1.start()
+                #t1.join()
+                
             else:
                 errorMessage = "Erro: ja existe um cliente com esse login na sala"
                 FSM_transmissor([b"ERROR", errorMessage.encode()], serverSocket, clientAdress)
